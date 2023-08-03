d6c218854bb396282bc84735c2a051f8abd2240d
Erdinc <erdincozdemir1812@gmail.com>
2023-08-03 18:58:58 +0300

    js 6 veri turleri

--- Lessons/04-javascript-kategorisi/js/006-veri-turu-ve-veri-turu-donusumleri.js
+++ Lessons/04-javascript-kategorisi/js/006-veri-turu-ve-veri-turu-donusumleri.js
@@ -1,4 +1,4 @@
-// ********** Veri Turunu Ogrenmek ve Veri Turu Donusumleri ********** 
+// ********** Veri Turunu Ogrenmek ve Veri Turu Donusumleri **********
 
 // veri turunu ogrenmek typeof:
 let price = 111
@@ -41,3 +41,56 @@ console.log("number4: ", number4, typeof(number4) )
 let number5 = 55
 number5 = number5.toString()
 console.log(number5, typeof(number5))
+
+
+console.log(typeof 42);
+// beklenen output: "number"
+
+console.log(typeof 'kodluyoruz');
+//beklenen output: "string"
+
+console.log(typeof true);
+// beklenen output: "boolean"
+
+console.log(typeof Variable);
+// beklenen output: "undefined"
+
+//isInteger( ) yöntemi, sayıların tam sayı olup olmadığını belirler.
+Number.isInteger(123) //true
+Number.isInteger(-123) //true
+Number.isInteger(0.5) //false
+
+//isFinite () yöntemi, bir değerin sonlu bir sayı olup olmadığını belirler.
+Number.isFinite(0) //true
+Number.isFinite('123') //false
+Number.isFinite('Hello') //false
+Number.isFinite(-Infinity) //false
+Number.isFinite(0 / 0) //false
+
+// Number.isNaN () yöntemi, bir değerin NaN (Not-A-Number) olup olmadığını belirler.
+
+Number.isNaN(123) //false
+Number.isNaN(0) //false
+Number.isNaN('123') //false
+Number.isNaN('Hello') //false
+Number.isNaN('') //false
+Number.isNaN(true) //false
+Number.isNaN(undefined) //false
+Number.isNaN('NaN') //false
+Number.isNaN(NaN) //true
+
+// Tahmin edebileceğiniz üzere her primitif değer stringe dönüştürülebilir.
+
+String(123) // “123”
+String(-12.3) // “-12.3”
+String(null) // “null”
+String(undefined) // “undefined”
+String(true) // “true”
+String(false) // “false
+
+
+// Nesneler (Objects) için Type Coercion
+console.log(Boolean({})) // true
+console.log(Boolean([])) // true
+console.log(Boolean([1,2,3])) // true
+console.log(Boolean({13:234})) //true
