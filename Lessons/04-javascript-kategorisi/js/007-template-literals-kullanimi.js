7343489c2abca8b1f264d43faedf74d36b745426
Erdinc <erdincozdemir1812@gmail.com>
2023-08-03 23:19:45 +0300

    js 7 templte literals

--- Lessons/04-javascript-kategorisi/js/007-template-literals-kullanimi.js
+++ Lessons/04-javascript-kategorisi/js/007-template-literals-kullanimi.js
@@ -29,3 +29,52 @@ let info = `Merhaba ${userName} sitemize hoşgeldin.. mail adresin: ${email} mai
 günün saat bilgisi : ${new Date().getHours()}
 kısa isminiz : ${userName[0]}.`
 console.log(info)
+
+// Çok Satırlı Dize Yazımı:
+//Eski kullanım:
+let eskiString = "En sevdiğim yemekler:\nİskender\nMantı"
+
+//Yeni kullanım:
+let yeniString = `En sevdiğim yemekler:
+İskender
+Mantı`
+
+// İnterpolasyon, metin içerisinde değişken kullanmak anlamına gelir. Normal dizeler içerisine ifadeler gömmek için kullanılır.
+
+let ad= `Erdinc`;
+let soyad= `Özdemir`;
+
+//Eski kullanım
+console.log("Benim adım "+ad+" "+ soyad);
+
+//Yeni kullanım
+console.log(`Benim adım ${ad} ${soyad}`);
+
+let a = 99;
+let b= 999;
+
+console.log(`${a} çarpı ${b} eşittir ${a*b}`);
+
+//Çıktı:
+//99 çarpı 999 eşittir 98901
+
+//Değişkenlerimizi tanımlayalım
+const gozRengi =`Kahverengi`
+const yas =21
+// → Eski kullanım, Template Literals kullanmadan:
+
+// const kisi = "<p>"+ad+"</p>"+
+//               "<p>"+soyad+"</p>"+
+//               "<p>"+gozRengi+"</p>"+
+//               "<p>"+yas+"</p>"
+
+// document.body.innerHTML = kisi;
+
+const kisi = `
+<p>${ad}</>
+<p>${soyad}</>
+<p>${gozRengi}</>
+<p>${yas}</>
+`;
+
+document.body.innerHTML = kisi;
