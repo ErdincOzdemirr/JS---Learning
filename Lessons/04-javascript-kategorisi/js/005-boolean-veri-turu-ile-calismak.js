849ac302b3a024b54d2c92fd8a0ef59bc6993681
Erdinc <erdincozdemir1812@gmail.com>
2023-08-03 18:05:07 +0300

    js 5 boolean

--- Lessons/04-javascript-kategorisi/js/005-boolean-veri-turu-ile-calismak.js
+++ Lessons/04-javascript-kategorisi/js/005-boolean-veri-turu-ile-calismak.js
@@ -1,28 +1,41 @@
-// ********** Boolean Veri Turu ile Calismak ********** 
+// ********** Boolean Veri Turu ile Calismak **********
 
 // 0 ve 1'i Anlamak ->
-let isActive = false // 0
-isActive = true // 1
-console.log(isActive)
+// let isActive = false // 0
+// isActive = true // 1
+// console.log(isActive)
 
 
-let userName;
-let isUserName = Boolean(userName)
-console.log(isUserName)
+// let userName;
+// let isUserName = Boolean(userName)
+// console.log(isUserName)
 
-Boolean("11") // true
-Boolean("0") // true
-Boolean("") // false
+// Boolean chrome consol da yazılırsa
+// Boolean("11") // true
+// Boolean("0") // true
+// Boolean("") // false
 
-userName = "user"
+// userName = "user"
 
-console.log("User Name:", Boolean(userName))
+// console.log("User Name:", Boolean(userName))
 
 // 0 ,-0, null, false, NaN, undefined, ("") ->
-Boolean(0) //  false
-Boolean(-0) // false
-Boolean(-0.1) // true
-Boolean(0 === 0) // true
+// Boolean(0) //  false
+// Boolean(-0) // false
+// Boolean(-0.1) // true
+// Boolean(0 === 0) // true
 
 // Not: Karar Yapilari Kisminda Tekrar Anlatilacak
-Boolean(userName.length > 0) // true
+// Boolean(userName.length > 0) // true
+
+// let isActive = false
+// isActive = true
+// console.log(isActive);
+
+// let userName;
+// let isUserName = Boolean(userName)
+// console.log(isUserName)
+
+// userName = "user"
+// console.log("User Name:", Boolean(userName))
+
