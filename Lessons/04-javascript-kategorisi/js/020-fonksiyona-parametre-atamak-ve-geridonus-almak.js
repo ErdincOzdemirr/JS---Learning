5ee9dbc590d6762ce555198748ea78eff4a936f6
Erdinc <erdincozdemir1812@gmail.com>
2023-08-04 21:59:34 +0300

    js 20 fonksiyon parametre atama

--- Lessons/04-javascript-kategorisi/js/020-fonksiyona-parametre-atamak-ve-geridonus-almak.js
+++ Lessons/04-javascript-kategorisi/js/020-fonksiyona-parametre-atamak-ve-geridonus-almak.js
@@ -5,35 +5,41 @@
 // 2: Bir Fonksiyon Disari Bilgi Gonderebilir(return) veya Gondermeyebilir
 // 3: Mumkunse Fonksiyonun Bagimliliklarini Azaltmak Gerekir
 
+let firstName = "Erdinc";
 
-let firstName = "Lorem"
+function greetings(firstName = "", lastName = "") {
+  // default parametre aliyor..
+  // console.log(`Merhaba ${firstName ? firstName : ""}`)
+  // console.log(`Merhaba ${firstName}`)
+  console.log(`Merhaba ${firstName} ${lastName}`);
+}
 
-function greetings(firstName="", lastName="") { // default parametre aliyor..
-    // console.log(`Merhaba ${firstName ? firstName : ""}`)
-    // console.log(`Merhaba ${firstName}`)
-    console.log(`Merhaba ${firstName} ${lastName}`)
-} 
-
-console.log(firstName) // degisken
-greetings() // fonksiyona parametre gondermedik ??
-greetings("Parametre")
+console.log(firstName); // degisken
+greetings(); // fonksiyona parametre gondermedik ??
+greetings("Parametre");
 
 function greetings2(firstName, lastName) {
-    let info = `Merhaba ${firstName} ${lastName}`
-    return info
+  let info = `Merhaba ${firstName} ${lastName}`;
+  return info;
 }
 
-let greetingsInfo = greetings2("Ad", "Soyad")
+let greetingsInfo = greetings2("Ad", "Soyad");
 // let info = "deneme" // ?????
-console.log(greetingsInfo)
+console.log(greetingsInfo);
 
 function domIdWriteInfo(id, info) {
-    let domObject = document.querySelector(`#${id}`)
-    domObject.innerHTML = info
+  let domObject = document.querySelector(`#${id}`);
+  domObject.innerHTML = info;
 }
 
-let htmlInfo = `<span style="color:red">Color REDDDD</span>`
+let htmlInfo = `<span style="color:red">Color REDDDD</span>`;
 
-domIdWriteInfo('greeting', htmlInfo)
-domIdWriteInfo('info', greetings2("Lorem", "Ipsum") )
+domIdWriteInfo("greeting", htmlInfo);
+domIdWriteInfo("info", greetings2("Lorem", "Ipsum"));
 
+// let firstName = "Lorem"
+// function greetings(firstName="") {
+    // console.log(`Merhaba ${firstName ? firstName : ""}`)
+//     console.log(`Merhaba ${firstName}`)
+// }
+// greetings()
