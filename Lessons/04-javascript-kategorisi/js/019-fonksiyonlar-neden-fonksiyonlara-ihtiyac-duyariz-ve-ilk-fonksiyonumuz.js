79d59f4755f76dd88dfc9a67bb59b045cbfcb9e0
Erdinc <erdincozdemir1812@gmail.com>
2023-08-04 21:27:27 +0300

    js 19 function

--- Lessons/04-javascript-kategorisi/js/019-fonksiyonlar-neden-fonksiyonlara-ihtiyac-duyariz-ve-ilk-fonksiyonumuz.js
+++ Lessons/04-javascript-kategorisi/js/019-fonksiyonlar-neden-fonksiyonlara-ihtiyac-duyariz-ve-ilk-fonksiyonumuz.js
@@ -3,15 +3,14 @@
 // function helloWorld() {
 //     console.log("Hello World!")
 // }
-function helloWorld(){
-    console.log("Hello World")
-}
-function hello() {
-    console.log("Merhaba")
-    helloWorld()
-}
-hello()  // calistir
-
+// function helloWorld(){
+//     console.log("Hello World")
+// }
+// function hello() {
+//     console.log("Merhaba")
+//     helloWorld()
+// }
+// hello()  // calistir
 
 // hata alabiliriz...
 // function userCheck () {
@@ -24,5 +23,58 @@ hello()  // calistir
 //     }
 // }
 
+// function printHello(name){                		 //name adında bir parametre aldı
+//     console.log(“Merhaba” + name);
+// }
+// printHello(“Şafak”);  // bir argüman vererek fonksiyonu çağırdık. Çıktı: Merhaba Şafak
 
+// function printScreen1 (){
+//     console.log(“İlk ekran çıktısı”);
+// }
 
+//  function printScreen2 (){
+//    setTimeout(function(){
+// console.log(“İkinci ekran çıktısı”)
+// }, 3000);
+// }
+
+// function printScreen3 (){
+//     console.log(“Üçüncü ekran çıktısı”);
+// }
+// printScreen1();
+// printScreen2();
+// printScreen3();
+
+// function mesajVer() {
+//     alert( 'Herkese Merhabalar!' );
+//     }
+// mesajVer()
+
+// function mesajVer(ad, soyad) {
+//   alert(`Merhaba ${ad} ${soyad}`);
+// }
+
+// mesajVer("Erdinc", "Ozdemir");
+
+// function topla(sayi1, sayi2) {
+//   return sayi1 + sayi2;
+// }
+
+// var sonuc = topla(10, 20);
+// console.log(sonuc);
+
+// function pow(x, n) {
+//   if (n == 1) {
+//     return x;
+//   } else {
+//     return x * pow(x, n - 1);
+//   }
+// }
+
+// alert(pow(2, 7)); // 8
+
+if (true) {
+    let message = 'Merhaba';
+    console.log(message);
+  }
+  console.log(message);
