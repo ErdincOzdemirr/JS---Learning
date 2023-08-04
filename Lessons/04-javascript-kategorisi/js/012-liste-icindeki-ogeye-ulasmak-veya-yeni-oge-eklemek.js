b86c6289fbce1633272b97f319789038a1699c20
Erdinc <erdincozdemir1812@gmail.com>
2023-08-04 16:24:10 +0300

    js 12 list element add 2

--- Lessons/04-javascript-kategorisi/js/012-liste-icindeki-ogeye-ulasmak-veya-yeni-oge-eklemek.js
+++ Lessons/04-javascript-kategorisi/js/012-liste-icindeki-ogeye-ulasmak-veya-yeni-oge-eklemek.js
@@ -1,18 +1,27 @@
 // ********** liste icerisindeki son elemana ulasmak veya eleman eklemek **********
 
-let lastChild = document.querySelector("ul#list>li:last-child")
-lastChild.innerHTML = "son oge degisti..."
+// let lastChild = document.querySelector("ul#list>li:last-child")
+// lastChild.innerHTML = "son oge degisti..."
 
-let firstChild = document.querySelector("ul#list>li:first-child")
-firstChild.innerHTML = "ilk oge degisti..."
+// let firstChild = document.querySelector("ul#list>li:first-child")
+// firstChild.innerHTML = "ilk oge degisti..."
 
-let ulDOM = document.querySelector("ul#list")
-let liDOM = document.createElement('li')
+// let ulDOM = document.querySelector("ul#list")
+// let liDOM = document.createElement('li')
 
-liDOM.innerHTML = "Kendi Olusturdugumuz Oge"
+// liDOM.innerHTML = "Kendi Olusturdugumuz Oge"
 
-ulDOM.append(liDOM) // en sona ekler
+// ulDOM.append(liDOM) // en sona ekler
 // ulDOM.prepend(liDOM)//en başa ekler
 
-let item = document.querySelector("ul#list>li:last-child")
-console.log(item)
+// let item = document.querySelector("ul#list>li:last-child")
+// console.log(item)
+
+//Document
+
+var one = document.getElementById("walterwhite")
+var two = document.getElementsByClassName("alternate")
+
+for ( var i = 0; i<two.length; i++) {
+    two[i].computedStyleMap.color = "red";
+}
