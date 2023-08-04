04de6b603e32ad21822ae144e9877eefb8c00462
Erdinc <erdincozdemir1812@gmail.com>
2023-08-04 15:36:36 +0300

    js 10 dom 3

--- Lessons/04-javascript-kategorisi/js/010-dom-icerisinden-oge-secimi.js
+++ Lessons/04-javascript-kategorisi/js/010-dom-icerisinden-oge-secimi.js
@@ -13,3 +13,15 @@
 // link.style.color = "red"
 // link.classList.add('btn')
 
+// let title = document.getElementsByTagName('h2')
+// console.log(title)
+// let h2 = document.getElementsByTagName('h2')
+// console.log(h2.title)
+
+// let title = document.getElementById('title')
+// console.log(title.innerHTML = "deneme")
+// title.innerHTML = "Degisen Bilgi"
+// console.timeLog(title.innerHTML)
+
+// let link = document.querySelector("ul#list>li>a");
+// console.log(link)
