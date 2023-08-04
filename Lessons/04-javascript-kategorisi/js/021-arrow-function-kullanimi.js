6c5b8be6ec775aeeee1b0ad541096f6cb3845552
Erdinc <erdincozdemir1812@gmail.com>
2023-08-04 22:59:58 +0300

    js 21 function fat arrow

--- Lessons/04-javascript-kategorisi/js/021-arrow-function-kullanimi.js
+++ Lessons/04-javascript-kategorisi/js/021-arrow-function-kullanimi.js
@@ -1,30 +1,40 @@
 // Arrow function Kullanimi
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
 
-function hello(firstName) {
-    console.log(`Merhaba ${firstName}`)
-}
+// function hello(firstName) {
+//     console.log(`Merhaba ${firstName}`)
+// }
 
-hello("Erdinc")
+// hello("Erdinc")
 
-const helloFuncV1 = (firstName) => { console.log(`Merhaba ${firstName}`) }
-helloFuncV1("helloFuncV1")
-
-
-const helloFuncV2 = firstName => console.log(`Merhaba ${firstName}`) // bir parametre, bir donus islemi
-helloFuncV2("helloFuncV2")
+// const helloFuncV1 = (firstName) => { console.log(`Merhaba ${firstName}`) }
+// helloFuncV1("helloFuncV1")
 
 
-const helloFuncV3 = (firstName, lastName) => console.log(
-    `Merhaba ${firstName} ${lastName}`
-) // console.log parantezi
+// const helloFuncV2 = firstName => console.log(`Merhaba ${firstName}`) // bir parametre, bir donus islemi
+// helloFuncV2("helloFuncV2")
 
-helloFuncV3("helloFuncV3", "Last Name info")
 
-const helloFuncV4 = (firstName, lastName) => {
-    let info = `Merhaba ${firstName} ${lastName}`
-    console.log(info)
-    return info
-}
+// const helloFuncV3 = (firstName, lastName) => console.log(
+//     `Merhaba ${firstName} ${lastName}`
+// ) // console.log parantezi
 
-helloFuncV4("helloFuncV4", "Other Info")
+// helloFuncV3("helloFuncV3", "Last Name info")
+
+// const helloFuncV4 = (firstName, lastName) => {
+//     let info = `Merhaba ${firstName} ${lastName}`
+//     console.log(info)
+//     return info
+// }
+
+// helloFuncV4("helloFuncV4", "Other Info")
+
+//Document
+// Function declaration
+// function greet(who) {
+//     return `Hello, ${who}`;
+// }
+// greet("Erdinc")
+// console.log(greet)
+
+// const greet = who => `Hello, ${who}!`;
