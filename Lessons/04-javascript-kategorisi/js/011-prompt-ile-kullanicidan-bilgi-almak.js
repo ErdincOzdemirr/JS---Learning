c4ad3fb1e196bad1e2ca33c1fb933643ef09f181
Erdinc <erdincozdemir1812@gmail.com>
2023-08-04 16:03:18 +0300

    js 11 prompt 4

--- Lessons/04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
+++ Lessons/04-javascript-kategorisi/js/011-prompt-ile-kullanicidan-bilgi-almak.js
@@ -1,9 +1,18 @@
 // ********** prompt ile Kullanicidan Bilgi Almak **********
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
 
-let fullName = prompt("Lutfen Adinizi Giriniz: ")
-console.log(fullName)
+// let fullName = prompt("Lutfen Adinizi Giriniz: ")
+// console.log(fullName)
 
-let greeting = document.querySelector("#greeting")
+// let greeting = document.querySelector("#greeting")
 
-greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
+// greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
+
+// function istek() {
+//     var kelime=prompt("Bir değer giriniz","lütfen sadece kelime giriniz..")
+//     document.write(kelime)
+//   }
+
+//   var a = prompt("Bir değer giriniz","lütfen sadece kelime giriniz..")
+var isim = prompt("Adınız nedir?");
+console.log("Merhaba, " + isim);
