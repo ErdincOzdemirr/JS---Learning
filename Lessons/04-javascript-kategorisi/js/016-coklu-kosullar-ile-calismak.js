30d1b78cdc4e7f41bddeaaf48d11fc1a83d26619
Erdinc <erdincozdemir1812@gmail.com>
2023-08-04 18:55:47 +0300

    js 16 if

--- Lessons/04-javascript-kategorisi/js/016-coklu-kosullar-ile-calismak.js
+++ Lessons/04-javascript-kategorisi/js/016-coklu-kosullar-ile-calismak.js
@@ -1,11 +1,11 @@
 // ********** Coklu Kosullarla Calismak **********
 
-let userName = prompt("Kullanici Adiniz:")
-let age = prompt("Yasiniz:")
+let userName = prompt("Kullanici Adiniz: ")
+let age = prompt("Yasiniz: ")
 let info = document.querySelector("#info")
 
 if (userName && age >= 18) {
-    info.innerHTML = "ehliyet alabilirsiniz"
+    info.innerHTML = "Ehliyet alabilirsiniz"
 } else if (!userName) {
     info.innerHTML = "Kullanici Adiniz Yok"
 } else if ( !(age >= 18) ) {
