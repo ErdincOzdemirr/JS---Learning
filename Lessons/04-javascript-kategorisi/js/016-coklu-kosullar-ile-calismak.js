801b5dcef50906aa8a01eaf69d7dc7963cf98c14
Erdinc <erdincozdemir1812@gmail.com>
2023-08-04 19:39:55 +0300

    js 16 if 4

--- Lessons/04-javascript-kategorisi/js/016-coklu-kosullar-ile-calismak.js
+++ Lessons/04-javascript-kategorisi/js/016-coklu-kosullar-ile-calismak.js
@@ -1,13 +1,36 @@
 // ********** Coklu Kosullarla Calismak **********
 
-let userName = prompt("Kullanici Adiniz: ");
-let age = prompt("Yasiniz: ");
-let info = document.querySelector("#info");
+// let userName = prompt("Kullanici Adiniz: ");
+// let age = prompt("Yasiniz: ");
+// let info = document.querySelector("#info");
 
-if (userName.length > 0 && age >= 18) {
-  info.innerHTML = "Ehliyet alabilirsiniz";
-} else if (!userName) {
-  info.innerHTML = "Kullanici Adiniz Yok";
-} else if (!(age >= 18)) {
-  info.innerHTML = "Yas Bilginiz Yok veya 18 Yasindan Kucuksunuz";
+// if (userName.length > 0 && age >= 18) {
+//   info.innerHTML = "Ehliyet alabilirsiniz";
+// } else if (!userName) {
+//   info.innerHTML = "Kullanici Adiniz Yok";
+// } else if (!(age >= 18)) {
+//   info.innerHTML = "Yas Bilginiz Yok veya 18 Yasindan Kucuksunuz";
+// }
+
+//Hava Durumu APP
+let hava = prompt(
+  "Hava Durumunu (Yagmurlu, Gunesli, Bulutlu, Karlı, Firtinali) giriniz)"
+);
+switch (hava) {
+  case "Yagmurlu":
+    console.log("Semsiyeni yanina almayi unutma");
+    break;
+  case "Gunesli":
+    console.log("Hafif giyin");
+  case "Bulutlu":
+    console.log("Disari cik");
+    break;
+  case "Karlı":
+    console.log("Kalin giyin");
+    break;
+  case "Firtinali":
+    console.log("Bir süre disari cikma");
+    break;
+  default:
+    console.log("Bilinmeyen hava durumu:", hava);
 }
