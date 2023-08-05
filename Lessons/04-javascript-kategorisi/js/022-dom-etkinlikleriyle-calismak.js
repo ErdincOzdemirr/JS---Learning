38c26abb42551dcc9918082c809e0cf95111e140
Erdinc <erdincozdemir1812@gmail.com>
2023-08-05 08:37:51 +0300

    js 22 dom and function

--- Lessons/04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ Lessons/04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -2,10 +2,15 @@
 // https://www.w3schools.com/jsref/dom_obj_event.asp
 // https://developer.mozilla.org/en-US/docs/Web/Events
 
-let greeting = document.querySelector("#greeting")
-greeting.addEventListener("click", domClick)
+let greeting = document.querySelector("#greeting");
+greeting.addEventListener("click", domClick);
+
+// function domClick() {
+//     console.log("etkinlik denetlendi")
+//     this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
+// }
 
 function domClick() {
-    console.log("etkinlik denetlendi")
-    this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
+  console.log("tıklandı");
+  console.log(this.innerHTML = "bilgi degisti")
 }
