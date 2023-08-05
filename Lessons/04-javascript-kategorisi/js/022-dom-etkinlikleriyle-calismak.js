7c45aa2f2aef5f226057be448f9e40444a928cd0
Erdinc <erdincozdemir1812@gmail.com>
2023-08-05 20:32:33 +0300

    JS 22 DOM event 3

--- Lessons/04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ Lessons/04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -5,15 +5,30 @@
 // let greeting = document.querySelector("#greeting");
 // greeting.addEventListener("click", domClick);
 
-let greeting = document.querySelector("#greeting");
-greeting.addEventListener("click", domClick);
+// let greeting = document.querySelector("#greeting");
+// greeting.addEventListener("click", domClick);
+// // function domClick() {
+// //     console.log("etkinlik denetlendi")
+// //     this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
+// // }
+
 // function domClick() {
-//     console.log("etkinlik denetlendi")
-//     this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
+//   console.log("etkinlik denetlendi");
+//   //   console.log((this.innerHTML = "Tıklandığı için bilgi ve rengi degisti"));
+//   this.style.color == "red" ? (this.style.color = "black") : (this.style.color = "red");
 // }
 
-function domClick() {
-  console.log("etkinlik denetlendi");
-  //   console.log((this.innerHTML = "Tıklandığı için bilgi ve rengi degisti"));
-  this.style.color == "red" ? (this.style.color = "black") : (this.style.color = "red");
-}
+// Event Bumbling
+const selectBox = document.querySelector("#selectColor");
+const button = document.querySelector("#button");
+const form = document.querySelector(".inputText");
+const cerceve = document.querySelector(".container");
+selectBox.addEventListener("click", function () {
+  console.log("selectBox");
+});
+form.addEventListener("click", function () {
+  console.log("form");
+});
+cerceve.addEventListener("click", function () {
+  console.log("cerceve");
+});
