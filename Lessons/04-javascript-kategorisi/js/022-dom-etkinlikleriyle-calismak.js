c09afddd0a5287e6de49fbd94b61ffb6729a9cd9
Erdinc <erdincozdemir1812@gmail.com>
2023-08-05 20:48:22 +0300

    js 23 dom function final project

--- Lessons/04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
+++ Lessons/04-javascript-kategorisi/js/022-dom-etkinlikleriyle-calismak.js
@@ -19,16 +19,16 @@
 // }
 
 // Event Bumbling
-const selectBox = document.querySelector("#selectColor");
-const button = document.querySelector("#button");
-const form = document.querySelector(".inputText");
-const cerceve = document.querySelector(".container");
-selectBox.addEventListener("click", function () {
-  console.log("selectBox");
-});
-form.addEventListener("click", function () {
-  console.log("form");
-});
-cerceve.addEventListener("click", function () {
-  console.log("cerceve");
-});
+// const selectBox = document.querySelector("#selectColor");
+// const button = document.querySelector("#button");
+// const form = document.querySelector(".inputText");
+// const cerceve = document.querySelector(".container");
+// selectBox.addEventListener("click", function () {
+//   console.log("selectBox");
+// });
+// form.addEventListener("click", function () {
+//   console.log("form");
+// });
+// cerceve.addEventListener("click", function () {
+//   console.log("cerceve");
+// });
--- Lessons/04-javascript-kategorisi/js/023-functions-bolum-sonu-egzersizi.js
+++ Lessons/04-javascript-kategorisi/js/023-functions-bolum-sonu-egzersizi.js
@@ -1,17 +1,17 @@
 // Functions Bolum Sonu Egzersizi
 
-let counter = 0
-let counterDOM = document.querySelector('#counter')
-let increaseDOM = document.querySelector('#increase')
-let decreaseDOM = document.querySelector('#decrease')
+let counter = 0;
+let counterDOM = document.querySelector("#counter");
+let increaseDOM = document.querySelector("#increase");
+let decreaseDOM = document.querySelector("#decrease");
 
-counterDOM.innerHTML = counter
+counterDOM.innerHTML = counter;
 
-increaseDOM.addEventListener("click", clickEvent)
-decreaseDOM.addEventListener("click", clickEvent)
+increaseDOM.addEventListener("click", clickEvent);
+decreaseDOM.addEventListener("click", clickEvent);
 
 function clickEvent() {
-    console.log(this.id)
-    this.id == "increase" ?  counter += 1 : counter -= 1
-    counterDOM.innerHTML = counter
+  console.log(this.id);
+  this.id == "increase" ? (counter += 1) : (counter -= 1);
+  counterDOM.innerHTML = counter;
 }
