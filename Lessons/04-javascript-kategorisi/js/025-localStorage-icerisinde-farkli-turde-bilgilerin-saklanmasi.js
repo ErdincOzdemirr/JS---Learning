d7eb18e202d4be739503afdf5f8d168bf171ef12
Erdinc <erdincozdemir1812@gmail.com>
2023-08-09 23:24:23 +0300

    js 25 local storage 9

--- Lessons/04-javascript-kategorisi/js/025-localStorage-icerisinde-farkli-turde-bilgilerin-saklanmasi.js
+++ Lessons/04-javascript-kategorisi/js/025-localStorage-icerisinde-farkli-turde-bilgilerin-saklanmasi.js
@@ -1,19 +1,17 @@
 // Local Storage Islemleri:
 // Eger tutacagimiz bilgi object ise JSON.stringify ile objeyi string yapiya cevirebiliriz.
 
-let user = {userName: "erdincozdemir...", isActive: true}
-console.log(user)
-localStorage.setItem('userInfo', JSON.stringify(user) )
+let user = { userName: "erdincozdemir..", isActive: true };
+console.log(user);
+localStorage.setItem("userInfo", JSON.stringify(user));
 
-
-let userInfo = localStorage.getItem('userInfo') // bilgiyi al
-userInfo = JSON.parse(userInfo)
-console.log(userInfo)
-
+let userInfo = localStorage.getItem("userInfo"); // bilgiyi al
+userInfo = JSON.parse(userInfo);
+console.log(userInfo);
 
 // 2. egzersiz:
-let items = [1,2,3, user]
+let items = [1, 2, 3, user];
 // localStorage.setItem('newItems', items) // hatali :(
-localStorage.setItem('newItems', JSON.stringify(items))
+localStorage.setItem("newItems", JSON.stringify(items));
 
-let newItems = JSON.parse( localStorage.getItem('newItem') ) //JSON Parse ile localStorage icindeki bilginin cekilip kullanilmasi..
+let newItems = JSON.parse(localStorage.getItem("newItem")); //JSON Parse ile localStorage icindeki bilginin cekilip kullanilmasi..
