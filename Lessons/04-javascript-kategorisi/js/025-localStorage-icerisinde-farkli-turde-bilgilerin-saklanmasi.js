dd5340fe2dcfe7f4acdbbdd296b40f515f2e80ff
Erdinc <erdincozdemir1812@gmail.com>
2023-08-11 08:26:15 +0300

    js 25 localstorage

--- Lessons/04-javascript-kategorisi/js/025-localStorage-icerisinde-farkli-turde-bilgilerin-saklanmasi.js
+++ Lessons/04-javascript-kategorisi/js/025-localStorage-icerisinde-farkli-turde-bilgilerin-saklanmasi.js
@@ -3,15 +3,15 @@
 
 let user = { userName: "ErdincOzdemir", isActive: true };
 console.log(user);
-// localStorage.setItem("userInfo", JSON.stringify(user));
+localStorage.setItem("userInfo", JSON.stringify(user));
 
-// let userInfo = localStorage.getItem("userInfo"); // bilgiyi al
-// userInfo = JSON.parse(userInfo);
-// console.log(userInfo);
+let userInfo = localStorage.getItem("userInfo"); // bilgiyi al
+userInfo = JSON.parse(userInfo);
+console.log(userInfo);
 
 // 2. egzersiz:
 let items = [1, 2, 3, user];
-// localStorage.setItem('newItems', items) // hatali :(
+localStorage.setItem('newItems', items) // hatali :(
 localStorage.setItem("newItems", JSON.stringify(items));
 
 let newItems = JSON.parse(localStorage.getItem("newItem")); //JSON Parse ile localStorage icindeki bilginin cekilip kullanilmasi..
