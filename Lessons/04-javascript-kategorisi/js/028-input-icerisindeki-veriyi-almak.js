026c7cdcb100ad1a949c0768f7aa70b137f8aac8
Erdinc <erdincozdemir1812@gmail.com>
2023-08-11 08:59:12 +0300

    js 28 ınput value

--- Lessons/04-javascript-kategorisi/js/028-input-icerisindeki-veriyi-almak.js
+++ Lessons/04-javascript-kategorisi/js/028-input-icerisindeki-veriyi-almak.js
@@ -1,4 +1,4 @@
-// INPUT icindeki degeri(value) almak
+// js 28 INPUT icindeki degeri(value) almak
 
 let formDOM = document.querySelector("#userForm")
 formDOM.addEventListener('submit', formSubmit)
@@ -7,7 +7,7 @@ formDOM.addEventListener('submit', formSubmit)
 function formSubmit(event) {
     event.preventDefault() // default islemi engelledik...
     console.log("islem gerceklesti")
-    
+
     let scoreInputDOM = document.querySelector('#score')
     console.log(scoreInputDOM.value)
     localStorage.setItem('score', scoreInputDOM.value)
