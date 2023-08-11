f6f75701c19e80fcae1e1266b27185878244cc39
Erdinc <erdincozdemir1812@gmail.com>
2023-08-11 14:53:21 +0300

    js 34 while 2

--- Lessons/04-javascript-kategorisi/js/035-while-dongusu-kullanimi.js
+++ Lessons/04-javascript-kategorisi/js/035-while-dongusu-kullanimi.js
@@ -8,9 +8,36 @@
 //   counter ++
 // }
 
-let userName = ""
+// let userName = ""
 
-while (!userName) {
-  userName = prompt("Kullanici Adini Giriniz: ")
-  console.log(userName)
+// while (!userName) {
+//   userName = prompt("Kullanici Adini Giriniz: ")
+//   console.log(userName)
+// }
+
+// let counter = 0;
+// while (counter < 10) {
+//   console.log(counter)
+//   counter ++
+// }
+
+// let userName = "";
+// while (!userName) {
+//   userName = prompt("Kullanıcı Adini Giriniz: ");
+//   console.log(userName);
+// }
+
+// var degisken = 1;
+
+// while (degisken <= 10) {
+//   console.log("degiskenin degeri: " + degisken);
+
+//   degisken++;
+// }
+
+var arabalar = ["Honda", "Mercedes", "BMW", "FIAT", "RENAULT"];
+var i = 0;
+while (arabalar[i]) {
+  console.log(arabalar[i]);
+  i++;
 }
