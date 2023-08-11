7bde9e8d110d508fd79172258e09a8675c264cdc
Erdinc <erdincozdemir1812@gmail.com>
2023-08-11 17:35:24 +0300

    js to do list

--- Uygulama/java script to do list/todolist.js
+++ Uygulama/java script to do list/todolist.js
@@ -1,37 +1,88 @@
-//Element Ekleme Fonksiyonu:
-function newElement() {
-  const inputValue = document.getElementById("task").value;
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the list element and the todo list from local storage
+  let list = document.querySelector("#list");
+  let todoList = localStorage.getItem("todolist");
 
-  if (!inputValue) {
-    showToast("error", "Listeye boş ekleme yapamazsınız!");
-    return;
+  // Get the toasts and the add button elements
+  const toasts = document.querySelectorAll("#liveToast");
+  const btnadd = document.querySelector("#liveToastBtn");
+
+  // If there is a todo list in local storage, set the list element's inner HTML to it
+  if (todoList) {
+    list.innerHTML = JSON.parse(todoList);
   }
 
-  const list = document.getElementById("list");
-  const listItem = document.createElement("li");
-  listItem.textContent = inputValue;
+  // Function to create a new list item
+  function newElement() {
+    const li = document.createElement("li");
+    let task = document.querySelector("#task").value.trim();
 
-  // Yapıldı işareti için tıklama olayı ekle
-  listItem.addEventListener("click", toggleTask);
+    // If the task is empty, show the "Please enter a task" toast and return
+    if (!task) {
+      $(toasts[1]).toast("show");
+      return;
+    }
 
-  list.appendChild(listItem);
-  document.getElementById("task").value = "";
+    // Clear the input field
+    document.querySelector("#task").value = "";
 
-  showToast("success", "Listeye eklendi.");
-}
+    // Set the text content of the list item
+    li.textContent = task;
 
-//Görevi Tamamlandı Olarak İşaretleme Fonksiyonu:
-function toggleTask(event) {
-  const listItem = event.target;
-  listItem.classList.toggle("checked");
-}
+    // Add an event listener to toggle the "checked" class on click and update the todo list in local storage
+    li.addEventListener("click", () => {
+      if (li.classList.contains("checked")) li.classList.remove("checked");
+      else li.classList.add("checked");
+      localStorage.setItem("todolist", JSON.stringify(list.innerHTML));
+    });
 
-// Bootstrap Toast Bildirim Fonksiyonu:
-function showToast(type, message) {
-  const toast = document.querySelector(`.toast.${type}`);
-  const toastBody = toast.querySelector(".toast-body");
-  toastBody.textContent = message;
+    // Create a delete button and add an event listener to remove the list item
+    const delspan = document.createElement("span");
+    delspan.innerHTML = "&times;";
+    delspan.classList.add("close");
+    delspan.addEventListener("click", () => {
+      removeElement(li);
+    });
 
-  const toastInstance = new bootstrap.Toast(toast);
-  toastInstance.show();
-}
+    // Append the delete button to the list item and the list item to the list
+    li.appendChild(delspan);
+    list.appendChild(li);
+
+    // Show the "Task added to list" toast
+    $(toasts[0]).toast("show");
+
+    // Update the todo list in local storage
+    localStorage.setItem("todolist", JSON.stringify(list.innerHTML));
+  }
+
+  // Add an event listener to the input field to create a new list item on enter key press
+  document.querySelector("#task").addEventListener("keypress", (event) => {
+    if (event.key === "Enter") newElement();
+  });
+
+  // Add an event listener to each list item to toggle the "checked" class on click and update the todo list in local storage
+  document.querySelectorAll("#list li").forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.classList.contains("checked")) item.classList.remove("checked");
+      else item.classList.add("checked");
+      localStorage.setItem("todolist", JSON.stringify(list.innerHTML));
+    });
+  });
+
+  // Add an event listener to each delete button to remove the corresponding list item and update the todo list in local storage
+  document.querySelectorAll("#list span").forEach((item) => {
+    let li = item.parentNode;
+    item.addEventListener("click", () => {
+      removeElement(li);
+    });
+  });
+
+  // Add an event listener to the add button to create a new list item
+  btnadd.addEventListener("click", newElement);
+
+  // Function to remove a list item and update the todo list in local storage
+  function removeElement(li) {
+    li.remove();
+    localStorage.setItem("todolist", JSON.stringify(list.innerHTML));
+  }
+});
