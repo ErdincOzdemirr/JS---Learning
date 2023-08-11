44fd8ea4593ba1e9da140a581ca7d501e4fa7235
Erdinc <erdincozdemir1812@gmail.com>
2023-08-11 15:21:30 +0300

    JS 36 ARRAY FOR EACH 2

--- Lessons/04-javascript-kategorisi/js/036-array-forEach-kullanimi.js
+++ Lessons/04-javascript-kategorisi/js/036-array-forEach-kullanimi.js
@@ -2,12 +2,20 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/forEach
 // https://www.w3schools.com/jsref/jsref_foreach.asp
 
-const PRODUCTS = ["Macbook", "Phone", "Speaker", "Desktop PC", "Server", "Mouse", "Keyboard"]
+const PRODUCTS = [
+  "Macbook",
+  "Phone",
+  "Speaker",
+  "Desktop PC",
+  "Server",
+  "Mouse",
+  "Keyboard",
+];
 
 // PRODUCTS.forEach((product, index, array) => array[index] = product + " 111" )
-PRODUCTS.forEach((product, index, array) => array[index] = `${product.toUpperCase()}` )  // toUpperCase
+// PRODUCTS.forEach((product, index, array) => array[index] = `${product.toUpperCase()}` )  // toUpperCase
 
-console.log(PRODUCTS)
+// console.log(PRODUCTS)
 
 /*****
 const userListDOM = document.querySelector('#userList')
@@ -19,9 +27,19 @@ for (index = 0; index < users.length; index++) {
 }
 */
 
-const userListDOM = document.querySelector('#userList')
+// const userListDOM = document.querySelector('#userList')
 
-PRODUCTS.forEach( item => {
+// PRODUCTS.forEach( item => {
+//   const liDOM = document.createElement('li')
+//   liDOM.innerHTML = item
+//   userListDOM.append(liDOM)
+// })
+
+PRODUCTS.forEach((product, index, array) => array[index] = `${product.toUpperCase()}`);
+console.log(PRODUCTS);
+
+const userListDOM = document.querySelector('#userList');
+PRODUCTS.forEach ( item => {
   const liDOM = document.createElement('li')
   liDOM.innerHTML = item
   userListDOM.append(liDOM)
