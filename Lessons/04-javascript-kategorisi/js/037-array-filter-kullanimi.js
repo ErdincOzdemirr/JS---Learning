4e9cfc47bfd9bb61f370bca4a75a27e132aeb90d
Erdinc <erdincozdemir1812@gmail.com>
2023-08-11 15:28:01 +0300

    js 37 array Filter

--- Lessons/04-javascript-kategorisi/js/037-array-filter-kullanimi.js
+++ Lessons/04-javascript-kategorisi/js/037-array-filter-kullanimi.js
@@ -2,20 +2,27 @@
 // https://developer.mozilla.org/tr/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
 // 5 harften fazla olanlar (6) ??
-const PRODUCTS = ["Mic", "Cable", "Speaker", "Desktop PC", "Server", "Mouse", "Keyboard"]
+const PRODUCTS = [
+  "Mic",
+  "Cable",
+  "Speaker",
+  "Desktop PC",
+  "Server",
+  "Mouse",
+  "Keyboard",
+];
 
-const NEW_PRODUCTS = PRODUCTS.filter(item => item.length > 5)
-console.log(NEW_PRODUCTS)
-
+const NEW_PRODUCTS = PRODUCTS.filter((item) => item.length > 5);
+console.log(NEW_PRODUCTS);
 
 // aktif kullanicilar ??
 const USERS = [
-  {fullName: "Ayse Sumer", isActive: false},
-  {fullName: "Ahmet Urgan", isActive: true},
-  {fullName: "Asya Basar", isActive: true},
-  {fullName: "Aksel Durmaz", isActive: false},
-]
+  { fullName: "Erdinc Ozdemir", isActive: false },
+  { fullName: "Ahmet Urgan", isActive: true },
+  { fullName: "Asya Basar", isActive: true },
+  { fullName: "Aksel Durmaz", isActive: false },
+];
 
 // const ACTIVE_USERS = USERS.filter(user => user.isActive === true)
-const ACTIVE_USERS = USERS.filter(user => user.isActive )
-console.log(ACTIVE_USERS)
+const ACTIVE_USERS = USERS.filter((user) => user.isActive);
+console.log(ACTIVE_USERS);
