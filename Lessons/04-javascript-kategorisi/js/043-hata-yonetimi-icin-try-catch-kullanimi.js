f47f91eb4645178d3791a2edc1c5273ec0eef8ab
Erdinc <erdincozdemir1812@gmail.com>
2023-08-11 22:19:06 +0300

    js 43 try and catch

--- Lessons/04-javascript-kategorisi/js/043-hata-yonetimi-icin-try-catch-kullanimi.js
+++ Lessons/04-javascript-kategorisi/js/043-hata-yonetimi-icin-try-catch-kullanimi.js
@@ -2,19 +2,17 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
 // https://www.w3schools.com/js/js_errors.asp
 
-
 let products;
 
 try {
-  products.forEach(item => console.log(item));
+  products.forEach((item) => console.log(item));
 } catch (error) {
-  console.log("error:", error)
+  console.log("error:", error);
   // products = [1, 2, 3]
   // products.forEach(item => console.log(item));
 }
 
+console.log("hata yonetimi duzgun calisiyor");
 
-console.log("hata yonetimi duzgun calisiyor")
-
-let info = "kodluyoruz"
-console.log(info)
+let info = "Erdinc";
+console.log(info);
